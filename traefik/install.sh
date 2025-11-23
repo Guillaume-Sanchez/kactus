6b90637aa0491d472ac31dc804914830d067fba8
Guillaume-Sanchez <sanchez.guillaume116@gmail.com>
2025-11-23 13:48:41 +0100

    Traefik install version 1.2

--- traefik/install.sh
+++ traefik/install.sh
@@ -4,9 +4,8 @@
 # License: MIT | https://github.com/Guillaume-Sanchez/kactus
 
 # --- Configuration ---
-PROJECT_NAME="prometheus"
+PROJECT_NAME="traefik"
 COMPOSE_FILE="docker-compose.yml"
-CONF_FILE="prometheus.yml"
 
 echo "🚀 Préparation du projet Docker Compose..."
 
@@ -68,4 +67,4 @@ echo "🧹 Nettoyage des ressources ..."
 cd ..
 rm -rf $PROJECT_NAME
 
-echo "✨ Terminé ! Prometheus a bien été configurés et les ressources sont nettoyées."
+echo "✨ Terminé ! Traefik a bien été configurés et les ressources sont nettoyées."
