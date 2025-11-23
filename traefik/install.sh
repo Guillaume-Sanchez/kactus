42fe620120b0a68f99c85c869f6da098122adb76
Guillaume-Sanchez <sanchez.guillaume116@gmail.com>
2025-11-23 13:43:45 +0100

    Traefik install version 1

---
+++ traefik/install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Copyright (c) 2021-2025 Guillaume Sanchez
+# Author: Guillaume Sanchez
+# License: MIT | https://github.com/Guillaume-Sanchez/kactus
+
+# --- Configuration ---
+PROJECT_NAME="prometheus"
+COMPOSE_FILE="docker-compose.yml"
+CONF_FILE="prometheus.yml"
+
+echo "🚀 Préparation du projet Docker Compose..."
+
+# 1. Créer le répertoire du projet s'il n'existe pas
+mkdir -p $PROJECT_NAME
+cd $PROJECT_NAME
+
+# 2. Créer le fichier docker-compose.yml
+# Cette image exécute un binaire qui affiche le message et s'arrête.
+cat << EOF > $COMPOSE_FILE
+version: "3.8"
+
+services:
+   traefik:
+     image: traefik:latest
+    container_name: traefik
+     command:
+       - "--providers.docker=true"
+       - "--providers.docker.exposedbydefault=false"
+      
+       - "--entrypoints.web.address=:80"
+       - "--entrypoints.websecure.address=:443"
+
+       - "--certificatesresolvers.le.acme.httpchallenge=true"
+       - "--certificatesresolvers.le.acme.httpchallenge.entrypoint=web"
+       - "--certificatesresolvers.le.acme.email=sanchez.guillaume116@gmail.com"
+       - "--certificatesresolvers.le.acme.storage=/letsencrypt/acme.json"
+
+       - "--api.dashboard=true"
+     ports:
+       - "80:80"
+       - "443:443"
+     volumes:
+       - "/var/run/docker.sock:/var/run/docker.sock:ro"
+       - "./letsencrypt:/letsencrypt"
+     labels:
+       - "traefik.enable=true"
+       - "traefik.http.routers.traefik.rule=Host(`traefik.localhost`)"
+       - "traefik.http.routers.traefik.entrypoints=websecure"
+       - "traefik.http.routers.traefik.tls.certresolver=le"
+       - "traefik.http.routers.traefik.service=api@internal"
+EOF
+
+echo "✅ Fichier $COMPOSE_FILE créé dans $(pwd) :"
+cat $COMPOSE_FILE
+echo "---"
+# 3. Exécuter Docker Compose
+# -d pour détacher (pas nécessaire ici pour hello-world, mais bonne pratique)
+# --rm pour nettoyer le conteneur après l'arrêt (utile pour ce cas simple)
+
+echo "▶️ Lancement de 'docker compose up'"
+# On n'utilise pas -d pour voir la sortie immédiatement
+docker compose up --force-recreate --build --no-start
+docker compose start
+
+# 4. Nettoyage
+echo "🧹 Nettoyage des ressources ..."
+# Revenir au répertoire précédent et supprimer le répertoire de travail
+cd ..
+rm -rf $PROJECT_NAME
+
+echo "✨ Terminé ! Prometheus a bien été configurés et les ressources sont nettoyées."
