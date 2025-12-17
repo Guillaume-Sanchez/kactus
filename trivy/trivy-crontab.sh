ac23bdb5264b3efe5232875e287734e1893797a2
Guillaume-Sanchez <sanchez.guillaume116@gmail.com>
2025-12-17 23:02:46 +0100

    correction placement dans le dossier

--- trivy/trivy-crontab.sh
+++ trivy/trivy-crontab.sh
@@ -2,11 +2,13 @@
 # Copyright (c) 2021-2025 Guillaume Sanchez
 # Author: Guillaume Sanchez
 # License: MIT | https://github.com/Guillaume-Sanchez/kactus
-
+# version: 1.0.1
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 TRIVY_IMAGE="aquasec/trivy:latest"
 REPORT_DIR="./trivy-reports"
+
+cd ~/kactus/trivy
 
 echo "--- Début du scan de sécurité du $TIMESTAMP ---"
 IMAGES=$(docker ps --format "{{.Image}}")
