353eb0009ba5fb6a4d9a4f234a08e1a6c2b10b8f
Guillaume-Sanchez <sanchez.guillaume116@gmail.com>
2026-01-20 17:26:40 +0100

    Correction network

--- script/primo_install.sh
+++ script/primo_install.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2021-2025 Guillaume Sanchez
 # Author: Guillaume Sanchez
 # License: MIT | https://github.com/Guillaume-Sanchez/kactus
-# version: 1.0.0
+# version: 1.1.1
 
 echo "============================"
 echo "🚀 Clonage du dépôt GitHub de Kactus dans ~/kactus"
