5a1eaf67564045e7df0baad55dfee09246c9dda7
Guillaume-Sanchez <sanchez.guillaume116@gmail.com>
2026-02-10 18:52:21 +0100

    Modification init.sql pour juste créer un user phpipam

--- kactus-bdd/init.sql
+++ kactus-bdd/init.sql
@@ -3,9 +3,7 @@ CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'kactus'@'%' IDENTIFIED BY 'ZySS#gFcYR5AcOUr';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'kactus'@'%';
 
--- Création de la base et user PhpIPAM
-CREATE DATABASE IF NOT EXISTS phpipam;
+-- Création d'un'user PhpIPAM
 CREATE USER IF NOT EXISTS 'phpipam'@'%' IDENTIFIED BY 'mk!vT6PVYtmXPYAe';
-GRANT ALL PRIVILEGES ON phpipam.* TO 'phpipam'@'%';
 
 FLUSH PRIVILEGES;
